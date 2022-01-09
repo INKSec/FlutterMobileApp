f02813304285b443567657d109a64e6de64f389b
Jascha Hirsekorn <jascha.hirsekorn@student.fh-kiel.de>
2022-01-09 18:32:45 +0100

    added userdata model unit tests

---
+++ mob_app/test/storage/userdata_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mob_app/models/userdata.dart';
+
+void main() {
+  group("Userdata model test", () {
+    test("Should be able to create a userdata instance", () {
+      final userdata = UserData("displayName", "avatar", "serial");
+    });
+    test("Should be able to serialize a userdata instance", () {
+      final userdata = UserData("displayName", "avatar", "serial");
+      expect(userdata.serialize(), {
+        "displayName": "displayName",
+        "avatar": "avatar",
+        "serial": "serial"
+      });
+    });
+    test("Should be able to deserialize a userdata instance", () {
+      final userdata = UserData.deserialize({
+        "displayName": "displayName",
+        "avatar": "avatar",
+      }, "test");
+      expect(userdata.displayName, "displayName");
+      expect(userdata.avatar, "avatar");
+      expect(userdata.serial, "test");
+    });
+  });
+}
