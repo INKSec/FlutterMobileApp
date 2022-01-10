6f5fea246fd52da914cadf1635834b6bbca865b5
Jascha Hirsekorn <jascha.hirsekorn@student.fh-kiel.de>
2022-01-10 15:55:30 +0100

    make CrudMethoden testable

--- mob_app/lib/views/gaestebuch/crud.dart
+++ mob_app/lib/views/gaestebuch/crud.dart
@@ -3,12 +3,17 @@ import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CrudMethoden {
+  final FirebaseFirestore _firestore;
+
+  CrudMethoden({FirebaseFirestore? firestore})
+      : _firestore = firestore ?? FirebaseFirestore.instance;
+
   Future<void> addData(daten) async {
-    FirebaseFirestore.instance.collection("path").add(daten).catchError((e) {});
+    _firestore.collection("path").add(daten).catchError((e) {});
     print(e);
   }
 
   getData() async {
-    return await FirebaseFirestore.instance.collection("path").snapshots();
+    return await _firestore.collection("path").snapshots();
   }
 }
